00fb5858fd3abf3843c7fd935f42a8b5c7cdd5cb
Paul Roetman <proetman@gmail.com>
2017-11-15 18:43:01 +1000

    Initial load

--- setup/2017-11-12-aws-create-machine.py
+++ setup/2017-11-12-aws-create-machine.py
@@ -38,7 +38,7 @@
 
 # ### Constants and Env variables
 
-# In[4]:
+# In[16]:
 
 
 g_profile = 'us-east'
@@ -46,7 +46,7 @@ g_instance_type = 'm4.large'
 ami = 'ami-31ecfb26'             # only in us-east-1, virginia
 
 
-# In[14]:
+# In[17]:
 
 
 # Fixed constansts
@@ -54,7 +54,7 @@ g_name = 'fast-ai'
 g_cidr = '0.0.0/0'
 
 
-# In[15]:
+# In[18]:
 
 
 # Import libraries
@@ -75,7 +75,7 @@ def create_vpc(p_ec2_client):
 
     """
     # Find out if a VPC already exists for this range
-'Vpc.VpcId'
+
 
     
     
@@ -83,15 +83,99 @@ def create_vpc(p_ec2_client):
 
 # ## Main
 
-# In[ ]:
+# In[34]:
 
 
 session = boto3.Session(profile_name = g_profile)
 
+ec2 = boto3.resource('ec2')
+my_region = session.region_name
+
+print('Current region: {}'.format(my_region))
+
+filters = [{'Name':'tag:Name', 'Values':[g_name]}]
+
+print('List of available VPC')
+list(ec2.vpcs.filter(Filters=filters))
+print('END List of available VPC')
+
+
 ec2_client = session.client('ec2')
-response = ec2.describe_instances()
+
+# List all instances for this client
+response = ec2_client.describe_instances()
 print(response)
 
-l_vpc_id = create_vpc(ec2_client )
+
     
+
+
+# In[38]:
+
+
+get_ipython().run_line_magic('pinfo', 'ec2_client.modify_vpc_attribute')
+
+
+# In[45]:
+
+
+my_vpc = ec2_client.create_vpc(CidrBlock = '10.0.0.0/28')
+
+setval = [{'Value':True}]
+my_vpc['Vpc']['VpcId']
+
+# list(ec2_boto3.vpcs)
+
+
+# In[46]:
+
+
+ec2_client.modify_vpc_attribute(VpcId=my_vpc['Vpc']['VpcId'], EnableDnsSupport={'Value':True})
+
+
+# In[51]:
+
+
+ec2_client.modify_vpc_attribute(VpcId=my_vpc['Vpc']['VpcId'], EnableDnsHostnames={'Value':True})
+
+
+# ### Verify VPC parameters are correct
+
+# ### How to delete a VPC (incomplete)
+
+# In[66]:
+
+
+vpc_iterator = ec2.vpcs.all()
+for x in vpc_iterator:
+    print(x)
+    try:
+        x.delete()
+    except:
+        print('    Probably has dependencies, skipping needs more coding here!')
+
+# https://gist.github.com/neilswinton/d37787a8d84387c591ff365594bd26ed
+
+# Call EC2.Client.describe_vpc_endpoints. Filter on your VPC id.Call EC2.client.delete_vpc_endpoints on each
+
+# Call VPC.security_groups. Delete the group unless its group_name attribute is "main". The main security group will be deleted via VPC.delete().
+
+# Call EC2.Client.describe_vpc_peering_connections. Filter on your VPC id as the requester-vpc-info.vpc-id. (My VPC is a requester. There is also accepter-vpc-info.vpc-id among other filters.) Iterate through the entries keyed by VpcPeeringConnections. Get an instance of the peering connection by instantiating a EC2.ServiceResource.VpcPeeringConnection with the VpcPeeringConnectionId. Call VpcPeeringConnection.delete() to remove the peering connection.
+
+# Call vpc.route_tables.all() and iterate through the route tables. For each route table, iterate through its routes using the RouteTable.routes attribute. Delete the routes where route['Origin'] is 'CreateRoute'. I deleted using EC2.Client.delete_route using EC2.RouteTable.id and route['DestinationCidrBlock']. After removing the routes, call EC2.RouteTable.delete() to remove the route table itself. I set up exception handlers for each delete. Not every route table can be deleted, but I haven't cracked the code code. Maybe next week.
+
+# Iterate through vpc.network_acls.all(), test the NetworkAcl.is_default attribute and call NetworkAcl.delete for non-default acls.
+
+# Iterate through vpc.subnets.all().network_interfaces.all(). Call EC2.NetworkInterface.delete() on each.
+
+# Iterate through vpc.internet_gateways.all(). Call EC2.InternetGateway.delete() on each.
+
+# Call vpc.delete()
+        
+
+
+# In[62]:
+
+
+get_ipython().run_line_magic('pinfo', 'ec2.vpcs.all')
 
